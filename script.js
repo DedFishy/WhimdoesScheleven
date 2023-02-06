02fd933ac340b1102f50433e851919d8f65b4b1a
Boyney <54646090+SuperBoyne@users.noreply.github.com>
2023-02-06 13:06:34 -0500

    added stuff

--- script.js
+++ script.js
@@ -3,8 +3,15 @@ var startMenu = document.getElementById("startmenu");
 
 var showingStartMenu = false;
 
-startButton.onclick = function() {
+startButton.onclick = function(event) {
     showingStartMenu = !showingStartMenu;
     if (showingStartMenu) {startMenu.classList.add("startmenu-visible");}
     else {startMenu.classList.remove("startmenu-visible");}
+}
+
+document.getElementById("desktop").onclick = function(event) {
+    alert("E: " + event.target.id);
+    if (event.target.id != "startmenu") {
+    startMenu.classList.remove("startmenu-visible");
+    }
 }
