24aebeeafb09a0af081cca5ccd0691fce9c381a0
Boyney <54646090+SuperBoyne@users.noreply.github.com>
2023-02-06 13:15:46 -0500

    time

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 var startButton = document.getElementById("start-wrapper");
 var startMenu = document.getElementById("startmenu");
 
+var time = document.getElementById("time");
+var date = document.getElementById("date");
+
 var showingStartMenu = false;
 
 startButton.onclick = function(event) {
@@ -15,3 +18,17 @@ document.body.onclick = function(event) {
     startMenu.classList.remove("startmenu-visible");
     }
 }
+
+setInterval(function(){
+    var date = new Date();
+
+    var hours = date.getHours();
+
+    var ampm = hours > 12 ? "PM" : "AM";
+    if (hours > 12) hours -= 12;
+
+    var minutes = date.getMinutes();
+
+    time.innerHTML = hours + ":" + minutes + " " + ampm;
+},
+1000);
