f083a442544c12c29a45de63934e9347a8a96573
Boyney <54646090+SuperBoyne@users.noreply.github.com>
2023-02-06 13:22:06 -0500

    ---

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ startButton.onclick = function(event) {
 
 document.body.onclick = function(event) {
     if (!["start", "startmenu", "start-wrapper"].contains(event.target.id.toString())) {
-        alert(event.target.id + " " + event.target.id.equals("start"));
+        alert(event.target.id + " --- " + event.target.id.equals("start"));
         startMenu.classList.remove("startmenu-visible");
     }
 }
